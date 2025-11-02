3dad834099f0bb47c90b4aef24d3e8c418fe8f38
Никола Цветановски <nikolacvetanovski1996@outlook.com>
2025-11-02 16:29:27 +0100

    chore: remove unused redundant using directives for cleaner code

--- taskmaster-pro.WebApi/Controllers/HealthController.cs
+++ taskmaster-pro.WebApi/Controllers/HealthController.cs
@@ -1,17 +1,4 @@
-﻿using Features.Admin.Commands.ChangeUserRole;
-using Microsoft.AspNetCore.Authorization;
-using taskmaster_pro.Application.Features.Admin.Commands;
-using taskmaster_pro.Application.Features.Admin.Commands.ResetSecurityAttempts;
-using taskmaster_pro.Application.Features.Admin.Commands.UpdateUserRoles;
-using taskmaster_pro.Application.Features.Admin.Queries.GetAdminUserById;
-using taskmaster_pro.Application.Features.Admin.Queries.GetAllUsers;
-using taskmaster_pro.Application.Features.Admin.Queries.GetPagedUsers;
-using taskmaster_pro.Application.Features.Orders.Commands.DeleteOrder;
-using taskmaster_pro.Application.Features.Orders.Queries.PagedOrders;
-using taskmaster_pro.Application.Features.Schedules.Commands.DeleteSchedule;
-using taskmaster_pro.Application.Features.Schedules.Queries.PagedSchedules;
-
-namespace Controllers
+﻿namespace Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
